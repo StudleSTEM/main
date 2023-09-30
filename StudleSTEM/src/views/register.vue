78911b6491a62903314c91d5b0abd97b84df0251
robert <roberts.shalajevs@gmail.com>
2023-09-30 12:55:50 +0300

    login/registerV2

--- StudleSTEM/src/views/register.vue
+++ StudleSTEM/src/views/register.vue
@@ -1,31 +1,37 @@
 <template>
-    <div class="login-container">
-      <h2 class="login-heading">Register</h2>
-      <form @submit.prevent="register" class="login-form">
-        <div class="login-radio">
-          <div
-            class="radio-option"
-            :class="{ 'radio-option-active': userType === 'teacher' }"
-            @click="toggleUserType('teacher')"
-          >
-            <label class="radio-label">Teacher</label>
+    <div class="bg">
+      <div class="company-name">
+        <h1>StudleSTEM</h1>
+      </div>
+      <div class="login-container">
+        <h2 class="login-heading">Register</h2>
+        <form @submit.prevent="register" class="login-form">
+          <div class="login-radio">
+            <div
+              class="radio-option"
+              :class="{ 'radio-option-active': userType === 'teacher' }"
+              @click="toggleUserType('teacher')"
+            >
+              <label class="radio-label">Teacher</label>
+            </div>
+            <div
+              class="radio-option"
+              :class="{ 'radio-option-active': userType === 'student' }"
+              @click="toggleUserType('student')"
+            >
+              <label class="radio-label">Student</label>
+            </div>
           </div>
-          <div
-            class="radio-option"
-            :class="{ 'radio-option-active': userType === 'student' }"
-            @click="toggleUserType('student')"
-          >
-            <label class="radio-label">Student</label>
-          </div>
-        </div>
-        <label for="username" class="login-label">Username:</label>
-        <input type="text" id="username" v-model="username" class="login-input" />
-        <label for="email" class="login-label">Email:</label>
-        <input type="email" id="email" v-model="email" class="login-input" />
-        <label for="password" class="login-label">Password:</label>
-        <input type="password" id="password" v-model="password" class="login-input" />
-        <button type="submit" class="login-button">Register</button>
-      </form>
+          <label for="username" class="login-label">Username:</label>
+          <input type="text" id="username" v-model="username" class="login-input" />
+          <label for="email" class="login-label">Email:</label>
+          <input type="email" id="email" v-model="email" class="login-input" />
+          <label for="password" class="login-label">Password:</label>
+          <input type="password" id="password" v-model="password" class="login-input" />
+          <button type="submit" class="login-button">Register</button>
+        </form>
+        <button @click="goToLogin" class="loginn-button">Login</button>
+      </div>
     </div>
   </template>
   
@@ -41,7 +47,7 @@
     },
     methods: {
       toggleUserType(type) {
-        // Если выбран уже активный тип, сбрасываем выбор
+        // Toggle user type
         this.userType = this.userType === type ? null : type;
       },
       register() {
@@ -50,12 +56,19 @@
         console.log('Username:', this.username);
         console.log('Email:', this.email);
         console.log('Password:', this.password);
+      },
+      goToLogin() {
+        this.$router.push('/login');
       }
     }
   };
   </script>
   
   <style scoped>
+
+  .bg {
+    background-color: #6dc6cb;
+  }
   .login-container {
     background-color: #fcf9fb;
     padding: 20px;
@@ -92,25 +105,25 @@
     margin: 0 10px;
     border: 2px solid #6dc6cb;
     border-radius: 5px;
-    transition: background-color 0.3s ease; /* Добавление анимации для плавного изменения цвета фона */
+    transition: background-color 0.3s ease;
   }
   
   .radio-option:hover {
-    background-color: #f0f0f0; /* Цвет фона при наведении мышкой */
+    background-color: #f0f0f0;
   }
   
   .radio-option-active {
     border-color: #6dc6cb;
-    background-color: #4a90e2; /* Цвет фона для активного выбора */
+    background-color: #4a90e2;
     color: #fff;
   }
   
   .radio-option-active:hover {
-    background-color: #4a90e2; /* Цвет фона для активного выбора при наведении */
+    background-color: #4a90e2;
   }
   
   .radio-label {
-    pointer-events: none; /* Disable hover effects */
+    pointer-events: none;
   }
   
   .login-label {
@@ -135,6 +148,16 @@
   
   .login-button:hover {
     background-color: #6dc6cb;
+  }
+  
+  .company-name {
+    text-align: center;
+    margin-top: 50px;
+  }
+  
+  .company-name h1 {
+    color: #173744;
+    font-size: 48px;
   }
   </style>
   
