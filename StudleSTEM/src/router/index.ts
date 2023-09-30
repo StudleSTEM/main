6210a99bb79d973c5893f789d7b905790874ba4b
kristzz <emils.krists@gmail.com>
2023-09-30 12:14:48 +0300

    kipa nav bar routyeris broken

--- StudleSTEM/src/router/index.ts
+++ StudleSTEM/src/router/index.ts
@@ -1,18 +1,43 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router'
 
+import Teorija from '../views/Teorija.vue';
+import Uzdevumi from '../views/Uzdevumi.vue';
+import Testi from '../views/Testi.vue';
+import Profils from '../views/Profils.vue';
+
 const routes = [
   {
     path: '/',
     component: () => import('@/layouts/default/Default.vue'),
     children: [
       {
-        path: '',
+        path: '/',
         name: 'Home',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+      },
+      {
+        path: '/teorija',
+        name: 'teorija',
+        component: Teorija,
+      },
+      {
+        path: '/uzdevumi',
+        name: 'uzdevumi',
+        component: Uzdevumi,
+      },
+      {
+        path: '/testi',
+        name: 'testi',
+        component: Testi,
+      },
+      {
+        path: '/profils',
+        name: 'profils',
+        component: Profils,
       },
     ],
   },
--- StudleSTEM/src/views/Home.vue
+++ StudleSTEM/src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
+  <NavBar />
   <HelloWorld />
 </template>
 
 <script lang="ts" setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+  import HelloWorld from '@/components/HelloWorld.vue';
+  import NavBar from '@/components/NavBar.vue';
 </script>
