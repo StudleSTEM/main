2d76ac94aea0dff15af521ca2f3be7ce391b703d
robert <roberts.shalajevs@gmail.com>
2023-09-30 11:59:37 +0300

    login/register

--- StudleSTEM/src/router/index.ts
+++ StudleSTEM/src/router/index.ts
@@ -1,5 +1,7 @@
 // Composables
 import { createRouter, createWebHistory } from 'vue-router'
+import login from '../views/login.vue'
+import register from '../views/register.vue'
 
 const routes = [
   {
@@ -14,6 +16,16 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
       },
+      {
+        path: "/login",
+        name: "login",
+        component:  login
+      },
+      {
+        path: "/register",
+        name: "register",
+        component:  register
+      }
     ],
   },
 ]
---
+++ StudleSTEM/src/views/register.vue
@@ -0,0 +1,140 @@
+<template>
+    <div class="login-container">
+      <h2 class="login-heading">Register</h2>
+      <form @submit.prevent="register" class="login-form">
+        <div class="login-radio">
+          <div
+            class="radio-option"
+            :class="{ 'radio-option-active': userType === 'teacher' }"
+            @click="toggleUserType('teacher')"
+          >
+            <label class="radio-label">Teacher</label>
+          </div>
+          <div
+            class="radio-option"
+            :class="{ 'radio-option-active': userType === 'student' }"
+            @click="toggleUserType('student')"
+          >
+            <label class="radio-label">Student</label>
+          </div>
+        </div>
+        <label for="username" class="login-label">Username:</label>
+        <input type="text" id="username" v-model="username" class="login-input" />
+        <label for="email" class="login-label">Email:</label>
+        <input type="email" id="email" v-model="email" class="login-input" />
+        <label for="password" class="login-label">Password:</label>
+        <input type="password" id="password" v-model="password" class="login-input" />
+        <button type="submit" class="login-button">Register</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        userType: 'teacher', // Default to teacher
+        username: '',
+        email: '',
+        password: ''
+      };
+    },
+    methods: {
+      toggleUserType(type) {
+        // Если выбран уже активный тип, сбрасываем выбор
+        this.userType = this.userType === type ? null : type;
+      },
+      register() {
+        // Handle registration logic here
+        console.log('Registered as:', this.userType);
+        console.log('Username:', this.username);
+        console.log('Email:', this.email);
+        console.log('Password:', this.password);
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .login-container {
+    background-color: #fcf9fb;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    width: 300px;
+    margin: auto;
+    margin-top: 100px;
+  }
+  
+  .login-heading {
+    color: #173744;
+    text-align: center;
+    margin-bottom: 20px;
+  }
+  
+  .login-form {
+    display: flex;
+    flex-direction: column;
+  }
+  
+  .login-radio {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 10px;
+  }
+  
+  .radio-option {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    padding: 10px;
+    margin: 0 10px;
+    border: 2px solid #6dc6cb;
+    border-radius: 5px;
+    transition: background-color 0.3s ease; /* Добавление анимации для плавного изменения цвета фона */
+  }
+  
+  .radio-option:hover {
+    background-color: #f0f0f0; /* Цвет фона при наведении мышкой */
+  }
+  
+  .radio-option-active {
+    border-color: #6dc6cb;
+    background-color: #4a90e2; /* Цвет фона для активного выбора */
+    color: #fff;
+  }
+  
+  .radio-option-active:hover {
+    background-color: #4a90e2; /* Цвет фона для активного выбора при наведении */
+  }
+  
+  .radio-label {
+    pointer-events: none; /* Disable hover effects */
+  }
+  
+  .login-label {
+    color: #173744;
+    margin-bottom: 5px;
+  }
+  
+  .login-input {
+    padding: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #6dc6cb;
+  }
+  
+  .login-button {
+    background-color: #b9e379;
+    color: #173744;
+    border: none;
+    padding: 10px;
+    cursor: pointer;
+    border-radius: 5px;
+  }
+  
+  .login-button:hover {
+    background-color: #6dc6cb;
+  }
+  </style>
+  
